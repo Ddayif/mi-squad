eba89144d6655ab302f343455e158fcceb4d30a9
Joselin Grez <joselingrez.m@hotmail.es>
2017-05-06 23:10:24 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function MiembroSquad(nombre, apellido, edad, hobbies) {
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.edad = edad;
+	this.hobbies = hobbies;
+
+}
+
+function mostrar(){
+
+	var arr = [];
+
+	const Valentina = new MiembroSquad("Valentina", "Spuler", 21, ["Ver peliculas", "Dormir", "jugar con sus gatos"]);
+	const Martina = new MiembroSquad("Martina", "Covarrubias", 27, ["Cocinar", "Comer", "Andar en bicicleta"]);
+	
+}
